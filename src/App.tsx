134687636dc092a3b692b39bb18dc0c5c4272995
sayReine <sayinzogae.com>
2025-04-10 22:49:44 +0200

    just combined the cart page

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const App: React.FC = () => {
 
         {/* <Link to="/cart" className="text-lg">🛒 Cart</Link> */}
         <Link to="/cart" className="relative text-2xl">
-                🛒 Cart
+                🛒 
                 {totalItems > 0 && (
                   <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
                     {totalItems}
