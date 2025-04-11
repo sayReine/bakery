0d8bc1def97e13c78987670d40dafdc62afd8318
T-cynthia <tuyikundecynthia@gmail.co>
2025-04-11 19:50:47 +0200

    just added outfit

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,9 @@ const App: React.FC = () => {
 
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-gradient-to-br from-white to-pink-100">
+      <div className="min-h-screen flex flex-col bg-gradient-to-r from-blue-900 via-blue-600 to-blue-300 text-white">
+
+
 
         {/* Navbar */}
         <nav className="bg-white text-blue-900 p-4 flex justify-between items-center shadow-md">
@@ -108,7 +110,7 @@ const App: React.FC = () => {
 
             <Route path="/shop" element={
               <div className="max-w-6xl mx-auto p-4">
-                <h1 className="text-3xl font-bold mb-10 mt-10 text-blue-600">🛍️ Shop Our Collection</h1>
+                <h1 className="text-3xl font-bold mb-10 mt-10 text-white-600">🛍️ Shop Our Collection</h1>
                 <div className="flex flex-col md:flex-row gap-6">
                   {/* <FilterPanel
                     categories={allCategories}
